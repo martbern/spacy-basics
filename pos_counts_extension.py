1feb64e70d42d9beaccb9fc6c32b37be48640f94
Martin <martinbernstorff@gmail.com>
2021-08-20 12:50:34 +0200

    Add pos_count_extension

---
+++ pos_counts_extension.py
@@ -0,0 +1,44 @@
+from typing import Counter, Generator
+from spacy.tokens import Doc
+import spacy
+import os
+
+def file_generator(dir: str) -> Generator[str, None, None]:
+    for filename in os.listdir(dir):
+        if "danavis" in filename:
+            filepath = os.path.join(dir, filename)
+
+            with open(filepath, "r") as f:
+                yield f.read()
+
+def get_pos_counts(doc: Doc) -> Counter:
+    token_count = 0
+    pos_counts = Counter()
+
+    token_count += len(doc)
+            
+    for token in doc:
+        pos_counts[token.tag_] += 1
+
+    return pos_counts
+
+def main():
+    nlp = spacy.load("da_core_news_sm", exclude=["parser", "ner", "lemmatizer", "textcat"]) # Disable unused elements in the pipeline
+
+    pos_total = Counter()
+
+    for count, doc in  enumerate(nlp.pipe(file_generator(dir), batch_size=2, n_process=4)):
+        if count >= 10: # Arbitrary breakpoint for faster iteration in development
+            break
+    
+        pos_total += doc._.pos_counts
+    
+    print(pos_total)
+
+
+if __name__ == "__main__":
+    dir = "./danavis"
+
+    Doc.set_extension("pos_counts", getter=get_pos_counts)
+
+    main()
